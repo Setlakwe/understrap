5a023fb46d917f802dcba6b0b0d863e7d67546f3
Christian Baril <barilc@setlakwemode.com>
2018-08-30 14:31:27 -0400

    Added some spacing under title when on a page

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -8,34 +8,34 @@
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
-    <div class="text-center">
-    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-    </div>
-	</header><!-- .entry-header -->
+    <header class="entry-header">
+        <div class="text-center">
+            <?php the_title('<h1 class="entry-title ' . (is_single() ? 'single' : 'not-single') . '">', '</h1>'); ?>
+        </div>
+    </header><!-- .entry-header -->
     <!--<div class="text-center">
-        <?php /*echo get_the_post_thumbnail( $post->ID, 'large',array('class' => 'img-responsive ')  ); */?>
+        <?php /*echo get_the_post_thumbnail( $post->ID, 'large',array('class' => 'img-responsive ')  ); */ ?>
     </div>-->
-<!--    <div class="entry-meta">
-        <?php /*understrap_posted_on(); */?>
+    <!--    <div class="entry-meta">
+        <?php /*understrap_posted_on(); */ ?>
     </div>--><!-- .entry-meta -->
-	<div class="entry-content">
+    <div class="entry-content">
 
-		<?php the_content(); ?>
+        <?php the_content(); ?>
 
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
+        <?php
+        wp_link_pages(array(
+            'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
+            'after' => '</div>',
+        ));
+        ?>
 
-	</div><!-- .entry-content -->
+    </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
+    <footer class="entry-footer">
 
-		<?php understrap_entry_footer(); ?>
+        <?php understrap_entry_footer(); ?>
 
-	</footer><!-- .entry-footer -->
+    </footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
--- loop-templates/content.php
+++ loop-templates/content.php
@@ -10,52 +10,51 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 
-	<header class="entry-header">
+    <header class="entry-header">
         <div class="text-center">
-
-        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-		'</a></h2>' ); ?>
-            			<div class="entry-meta">
-            				<?php understrap_posted_on(); ?>
-            			</div>
+            <?php the_title(sprintf('<h2 class="entry-title ' . (is_single() ? 'single' : 'not-single') . '"><a href="%s" rel="bookmark">', esc_url(get_permalink())),
+                '</a></h2>'); ?>
+            <div class="entry-meta">
+                <?php understrap_posted_on(); ?>
+            </div>
 
         </div>
 
-        <?php  if ( ! is_single() ) { ?>
+        <?php if (!is_single()) { ?>
             <div class="text-center img-container">
-                <?php echo get_the_post_thumbnail( $post->ID, null ,array('class' => 'img-responsive ') ); ?>
+                <?php echo get_the_post_thumbnail($post->ID, null, array('class' => 'img-responsive ')); ?>
             </div>
-        <?php  } ?>
+        <?php } ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-<!--			<div class="entry-meta">-->
-<!--				--><?php //understrap_posted_on(); ?>
-<!--			</div> -->
+        <?php if ('post' == get_post_type()) : ?>
+            <!--			<div class="entry-meta">-->
+            <!--				--><?php //understrap_posted_on(); ?>
+            <!--			</div> -->
             <!-- .entry-meta -->
-		<?php endif; ?>
+        <?php endif; ?>
 
-	</header><!-- .entry-header -->
+    </header><!-- .entry-header -->
 
 
-	<div class="entry-content">
+    <div class="entry-content">
 
-		<?php
-		the_excerpt();
-		?>
+        <?php
+        the_excerpt();
+        ?>
 
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
+        <?php
+        wp_link_pages(array(
+            'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
+            'after' => '</div>',
+        ));
+        ?>
 
-	</div><!-- .entry-content -->
+    </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
+    <footer class="entry-footer">
 
-		<?php /*understrap_entry_footer(); */?>
+        <?php /*understrap_entry_footer(); */ ?>
 
-	</footer><!-- .entry-footer -->
+    </footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
